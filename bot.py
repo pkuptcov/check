9520ead7a0593e935045a393c92d1a5e767d187c
pkuptcov <pavel.kuptcov@gmail.com>
2018-03-21 10:15:25 +0300

    create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+#подключение необходимых библиотек
+import telebot
+import requests
+import time
+
+
+def check_status():
+    # идентификаторы бота и вашего с ним чата
+    token = '595308349:AAE9f0xyzRWc21o0jLlbiB5ixXgiQB8ilkA'
+    your_chat_id = 318882951
+
+    # функция проверки сайта
+    def check_200(hostname):
+        # создаем экземпляр бота
+        bot = telebot.TeleBot(token)
+        # делаем запрос к сайту
+        r = requests.get(hostname)
+        # если что-то не в порядке уведомляем нас об этом средствами бота
+        if r.status_code != 200:
+            bot.send_message(your_chat_id, ' Ошибка на сайте Петрович ' + str(r.status_code))
+        # возвращать нам ничего не обязательно
+        #pass
+
+        else:
+            bot.send_message(your_chat_id, ' Ошибка на сайте Петрович ' + str(r.status_code))
+
+    # применяем функцию проверки к целевому сайту
+    check_200("https://petrovich.ru/")
+    #check_200("https://b2b.stdp.ru/")
+
+    time.sleep(5)
+
+while True:
+    check_status()
