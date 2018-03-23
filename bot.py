a8100ffc5f1a33d2a05847d3909416bbbea5757b
pkuptcov <pavel.kuptcov@gmail.com>
2018-03-23 16:51:42 +0300

    update bot

--- bot.py
+++ bot.py
@@ -1,46 +1,28 @@
-#подключение необходимых библиотек
 import telebot
 import requests
 import time
 
 
-def check_status():
-    # идентификаторы бота и вашего с ним чата
-    token = '595308349:AAE9f0xyzRWc21o0jLlbiB5ixXgiQB8ilkA'
-    your_chat_id = 318882951
+token = '595308349:AAE9f0xyzRWc21o0jLlbiB5ixXgiQB8ilkA'
+your_chat_id = 318882951
+bot = telebot.TeleBot(token)
 
-    # функция проверки сайта
-    def check_petrovich(hostname):
-        # создаем экземпляр бота
-        bot = telebot.TeleBot(token)
-        # делаем запрос к сайту
-        r = requests.get(hostname)
-        # если что-то не в порядке, уведомляем нас об этом средствами бота
-        if r.status_code != 200:
-            bot.send_message(your_chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Петрович')
-        # возвращать нам ничего не обязательно
-        pass
-        # else:
-            # bot.send_message(your_chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Петрович')
 
-    def check_b2b(hostname):
-        # создаем экземпляр бота
-        bot = telebot.TeleBot(token)
-        # делаем запрос к сайту
-        r = requests.get(hostname)
-        # если что-то не в порядке уведомляем нас об этом средствами бота
-        if r.status_code != 200:
-            bot.send_message(your_chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Б2Б')
-        # возвращать нам ничего не обязательно
-        pass
-        # else:
-            # bot.send_message(your_chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Б2Б')
+def check_petrovich():
+    r = requests.get("https://petrovich.ru/")
+    if r.status_code != 200:
+        bot.send_message(your_chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Петрович')
+    pass
 
-    # применяем функцию проверки к целевому сайту
-    check_petrovich("https://petrovich.ru/")
-    check_b2b("https://b2b.stdp.ru/")
+
+def check_b2b():
+    r = requests.get("https://b2b.stdp.ru/")
+    if r.status_code != 200:
+        bot.send_message(your_chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Б2Б')
+    pass
 
     time.sleep(10)
 
 while True:
-    check_status()
+    check_petrovich()
+    check_b2b()
