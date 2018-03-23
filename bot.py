f0301e51329c4cfc591f72346cd724f751fae70b
pkuptcov <pavel.kuptcov@gmail.com>
2018-03-23 17:02:17 +0300

    update bot

--- bot.py
+++ bot.py
@@ -4,21 +4,21 @@ import time
 
 
 token = '595308349:AAE9f0xyzRWc21o0jLlbiB5ixXgiQB8ilkA'
-your_chat_id = 318882951
+chat_id = 318882951
 bot = telebot.TeleBot(token)
 
 
 def check_petrovich():
     r = requests.get("https://petrovich.ru/")
     if r.status_code != 200:
-        bot.send_message(your_chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Петрович')
+        bot.send_message(chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Петрович')
     pass
 
 
 def check_b2b():
     r = requests.get("https://b2b.stdp.ru/")
     if r.status_code != 200:
-        bot.send_message(your_chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Б2Б')
+        bot.send_message(chat_id, 'Ошибка ' + str(r.status_code) + ' на сайте Б2Б')
     pass
 
     time.sleep(10)
