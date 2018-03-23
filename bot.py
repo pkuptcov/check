9127638ce48226967a40f10e10d66397ed6a950f
pkuptcov <pavel.kuptcov@gmail.com>
2018-03-23 17:44:50 +0300

    update bot

--- bot.py
+++ bot.py
@@ -24,11 +24,11 @@ def check_b2b():
 
 def check_time():
     r = requests.get("https://b2b.stdp.ru/").elapsed.total_seconds()
-    if r > 0.1:
+    if r > 0.3:
         bot.send_message(chat_id, 'Время ответа от сервера ' + str(r) + ' s')
     pass
 
-    time.sleep(10)
+    time.sleep(5)
 
 while True:
     check_petrovich()
